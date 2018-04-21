52eedcd6dd5dc1ce1080541de1a7f12a1b68fe77
Will Kunkel <will@wkunkel.com>
2018-04-20 21:01:59 -0400

    Remove travis_retry from shell scripts, as it isn’t available there.

--- travis/install-stack.sh
+++ travis/install-stack.sh
@@ -2,4 +2,5 @@
 
 set -ex
 mkdir -p $HOME/.local/bin
-travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C $HOME/.local/bin '*/stack'
+curl -L https://www.stackage.org/stack/linux-x86_64 |
+  tar xz --wildcards --strip-components=1 -C $HOME/.local/bin '*/stack'
